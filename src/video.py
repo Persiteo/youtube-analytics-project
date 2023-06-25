298c5065538a0a980f5b6381f54967ef3ab766bb
Persiteo <Persiteo@users.noreply.github.com>
2023-06-25 22:19:59 +0300

    modified channel.py: added some __methods__

---
+++ src/video.py
@@ -0,0 +1,32 @@
+import os
+from googleapiclient.discovery import build
+
+
+class Video:
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
+
+    def __init__(self, video_id):
+        parts = 'snippet,statistics'
+        result = self.youtube.videos().list(id=video_id, part=parts).execute()
+        statistics = result['items'][0]['statistics']
+
+        self.video_id = video_id
+        self.id = video_id
+        self.title = result['items'][0]['snippet']['title']
+        self.url = f"https://www.youtube.com/channel/{self.video_id}"
+        self.views_count = statistics['viewCount']
+        self.likes_count = statistics['likeCount']
+
+    def __str__(self):
+        return self.title
+
+
+class PLVideo (Video):
+
+    def __init__(self, video_id, playlist_id):
+        super().__init__(video_id)
+        self.playlist_id = playlist_id
+
+    def __str_(self):
+        return self.title
